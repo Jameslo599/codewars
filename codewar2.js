4330c039ca2944314a0cd59361c0be1c0bd8dc02
James Lo <jameslo599@gmail.com>
2023-04-23 00:18:24 -0500

    codewar 2 kata improvement

--- codewar2.js
+++ codewar2.js
@@ -1,9 +1,5 @@
 //Opposite Number
 
 function opposite(number) {
-    if (number < 0) {
-      return number - (number * 2);
-    } else if (number > 0) {
-      return number - (number * 2);
-    } else {return 0;}
+    return(-number);
 }
