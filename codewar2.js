ea5179ab2ef100700445d7c8682e8b018180cc56
James Lo <jameslo599@gmail.com>
2023-04-22 23:48:45 -0500

    opposite number kata

---
+++ codewar2.js
@@ -0,0 +1,9 @@
+//Opposite Number
+
+function opposite(number) {
+    if (number < 0) {
+      return number - (number * 2);
+    } else if (number > 0) {
+      return number - (number * 2);
+    } else {return 0;}
+}
