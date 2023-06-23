5f85a5f9dd9e2a96703a3a8ae168cb9d36f32ab5
James Lo <jameslo599@gmail.com>
2023-06-23 00:32:07 -0500

    build tower kata

---
+++ challenges/codewar48.js
@@ -0,0 +1,22 @@
+
+const towerBuilder = nFloors => {
+    const arr = [];
+    
+    for (let i = 0; i < nFloors; i++) {
+      let floor = '*';
+      arr.push(floor += floor.repeat(i * 2));
+    }
+    
+    return arr.map((e, i) => {
+      return e.padStart((arr[arr.length - 1].length - e.length) / 2 + e.length).padEnd(arr[arr.length - 1].length);
+    })
+  }
+  
+  // Pseudocode
+  
+  // function(number of floors) {
+  //   loop through equal to the number of floors. The formula for constructing a tower is
+  //   floor = '*' and every iteration we do floor += '**'.
+    
+  //   return array
+  // }
