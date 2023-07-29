fa13f5af89d2629c7689f12eb98828ac26d4c763
James Lo <jameslo599@gmail.com>
2023-07-29 00:53:05 -0500

    replicate new

---
+++ challenges/codewar62.js
@@ -0,0 +1,13 @@
+//Replicate 'new'
+
+const nouveau = (Constructor, ...arg) => {
+  const instance = Object.create(Constructor.prototype);
+  const obj = Constructor.call(instance);
+  if (obj === null) return instance;
+  Constructor.apply(instance, arg);
+
+  return typeof obj !== "object" && typeof obj !== "function" ? instance : obj;
+};
+
+// Easiest Solution
+// let instance = Reflect.construct(Constructor, arg);
