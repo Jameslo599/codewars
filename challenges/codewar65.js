fa60b70a0c660ec0888612974ff41f4867fbe5f3
James Lo <jameslo599@gmail.com>
2023-09-20 02:25:47 -0500

    closures and class state kata

---
+++ challenges/codewar65.js
@@ -0,0 +1,27 @@
+// Using Closures to Share Class State
+
+var Cat = (function () {
+  const _weight = [];
+  const averageWeight = () => {
+    return _weight.reduce((acc, curr) => acc + curr) / _weight.length;
+  };
+  function CatConstructor(name, argWeight) {
+    if (name === undefined || argWeight === undefined) throw "error";
+    this.name = name;
+    _weight.push(argWeight);
+
+    Object.defineProperty(this, "weight", {
+      get() {
+        return argWeight;
+      },
+      set(newValue) {
+        const oldIndex = _weight.findIndex((e) => e === argWeight);
+        _weight[oldIndex] = newValue;
+        argWeight = newValue;
+      },
+      configurable: true,
+    });
+  }
+  CatConstructor.averageWeight = averageWeight;
+  return CatConstructor;
+})();
