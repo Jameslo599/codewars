a909815f14137ee6d2963cb16c3e31288f98db9f
James Lo <jameslo599@gmail.com>
2024-07-10 01:33:42 -0500

    gfe1

--- challenges/gfe1.js
+++ challenges/gfe1.js
@@ -8,17 +8,8 @@
 export default function fill(array, value, start = 0, end = array.length) {
   const length = array.length;
 
-  if (start < 0) {
-    start = -start > length ? 0 : length + start;
-  }
-
-  if (end > length) {
-    end = length + 1;
-  }
-
-  if (end < 0) {
-    end += length;
-  }
+  if (start < 0) start = start < -length ? 0 : start + length;
+  if (end < 0) end = end + length;
 
   for (let i = start; i < Math.min(end, length); i++) {
     array[i] = value;
